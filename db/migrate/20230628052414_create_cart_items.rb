f323d12d0da410075727f56a2971d122204a805f
nakazawa <zhongzejiangji@gmail.com>
2023-07-04 01:44:30 +0000

    admin_item_edit,delete add

--- db/migrate/20230628052414_create_cart_items.rb
+++ db/migrate/20230628052414_create_cart_items.rb
@@ -3,7 +3,7 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
     create_table :cart_items do |t|
 
       t.integer :customer_id,   null: false
-      t.integer :menu_id,       null: false
+      t.integer :item_id,       null: false
       t.integer :item_quantity, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2023_07_03_004347) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.integer "menu_id", null: false
+    t.integer "item_id", null: false
     t.integer "item_quantity", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
