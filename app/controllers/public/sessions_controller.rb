e893efc99f61913ecc1a2a6ee42d945045e7b8ee
nakazawa <zhongzejiangji@gmail.com>
2023-07-04 06:05:33 +0000

    public customer_status

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,19 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  # ↓ログイン機能が実行される前に実行する
+  before_action :reject_inactive_customer, only: [:create]
+
+  # 退会しているかを判断するメソッド。入力された情報とデータベースの会員情報を照らし合わせstatusがfalseの場合、ログインができない。
+  def reject_inactive_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && !@customer.status
+        flash[:danger] = 'お客様は退会済みです。申し訳ございませんが、別のメールアドレスをお使いください。'
+        redirect_to new_customer_session_path
+      end
+    end
+  end
 
   # GET /resource/sign_in
   # def new
