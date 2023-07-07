32c428e083b0d05984ff2b878b427ba61f14111a
nakazawa <zhongzejiangji@gmail.com>
2023-07-07 23:01:44 +0000

    order status add

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,23 @@
 class Admin::OrdersController < ApplicationController
+
   def show
+    @order = Order.find(params[:id])
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+    if @order.update(order_params)
+      @order_details.update_all(production_status: 1) if @order.status == "confirm_payment"
+      ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+    end
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
